519606694540c1376aceecb77a6e916175833c72
Juan Carlos <105291091+juancfp@users.noreply.github.com>
2026-02-16 19:41:20 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "GoABSDK",
             url: "https://devs.goab.io/ios/releases/latest/goabSdkIos.zip",
-            checksum: "7a1cbee70d7e2aa67ba1554064e8617e04dd77db5288eab3fe99315e17c941d0"
+            checksum: "d7c452c9b9716b35361d5b3367e9785a528ecc156e235a821eac0598c7a5a908"
         ),
     ]
 )
